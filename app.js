6c7b5be2910301c507c5cbd873c6d46a1bf57eed
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-01-16 22:47:39 +0000

    very basic data binding working

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import * as d3 from 'd3';
 
-d3.line();
-window.console.log('Hello 5');
+const data = [10, 3, 5, 6, 7, 3, 8, 9];
+
+d3.select('#thing')
+    .data(data)
+    .enter()
+    .append('div')
+    .html(d => d);
