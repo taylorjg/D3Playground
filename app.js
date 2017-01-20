4ca58e08c932b8638213b853313cf1ed4441f065
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-01-20 19:34:16 +0000

    Example 1 now uses random data and does removal of exit selection

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 import $ from 'jquery';
 import * as d3 from 'd3';
 
-function updateExample1(data) {
-    d3.selectAll('#example1 #dataArea')
-        .selectAll('div')
-        .data(data)
-        .enter()
-        .append('div')
-        .html(d => d);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-$('#setDataExample1Btn').click(() => updateExample1([1, 2, 3, 4, 5]));
+function range(numValues) {
+    return Array.from(Array(numValues).keys());
+}
+
+function createRandomData(minValues, maxValues, maxValue) {
+    const numValues = getRandomInt(minValues, maxValues + 1);
+    return range(numValues).map(() => getRandomInt(0, maxValue + 1));
+}
+
+function updateExample1(data) {
+    function update(selection) {
+        selection.text((d, i) => `index: ${i}: ${d}`);
+    }
+    const selection = d3.selectAll('#example1Visualisation')
+        .selectAll('div')
+        .data(data);
+    update(selection);
+    update(selection.enter().append('div'));
+    selection.exit().remove();
+    d3.select('#example1RawData').text(`[${data.join(', ')}]`);
+}
+
+$('#setDataExample1Btn').click(() => updateExample1(createRandomData(1, 10, 50)));
