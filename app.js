c7663be55796efc3b8c82d5785c6345f3fecb6da
Jon Taylor <jont@cakesolutions.net>
2017-01-20 09:07:44 +0000

    Got first basic example working

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 import $ from 'jquery';
 import * as d3 from 'd3';
 
-const example1 = d3.select('#example1');
-
-let data1 = [10, 3, 5, 6, 7, 3, 8, 9];
-
-$('#changeDataBtn').click(() => {
-    window.console.log('click!');
-    data1 = [1, 2, 3, 4, 5];
-    updateExample1();
-});
-
-updateExample1();
-
-function updateExample1() {
-    example1
-        .data(data1)
+function updateExample1(data) {
+    d3.selectAll('#example1')
+        .selectAll('div')
+        .data(data)
         .enter()
         .append('div')
         .html(d => d);
-
 }
+
+$('#setDataBtn').click(() => updateExample1([1, 2, 3, 4, 5]));
