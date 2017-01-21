1e702e3d466ca91f024841f72fce7a7ab37ccab0
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-01-21 11:30:26 +0000

    Example2 - scales, colours, etc.

--- app.js
+++ app.js
@@ -15,24 +15,69 @@ function createRandomData(minValues, maxValues, maxValue) {
     return range(numValues).map(() => getRandomInt(0, maxValue + 1));
 }
 
-const example1 = d3.select('.example1');
+/*
+ * Example 1
+ */
 
-function updateExample1(data) {
-    function update(selection) {
-        selection.text((d, i) => `index: ${i}: ${d}`);
+(function () {
+    const example = d3.select('.example1');
+
+    function updateExample(data) {
+        function update(selection) {
+            selection.text((d, i) => `index: ${i}: ${d}`);
+        }
+        const selection = example
+            .select('.visualisation')
+            .selectAll('div')
+            .data(data);
+        update(selection);
+        update(selection.enter().append('div'));
+        selection.exit().remove();
+        example
+            .select('.rawData')
+            .text(`[${data.join(', ')}]`);
     }
-    const selection = example1
-        .selectAll('.visualisation')
-        .selectAll('div')
-        .data(data);
-    update(selection);
-    update(selection.enter().append('div'));
-    selection.exit().remove();
-    example1
-        .select('.rawData')
-        .text(`[${data.join(', ')}]`);
-}
 
-example1
-    .select('.setDataBtn')
-    .on('click', () => updateExample1(createRandomData(1, 10, 50)));
+    example
+        .select('.setDataBtn')
+        .on('click', () => updateExample(createRandomData(1, 10, 50)));
+} ());
+
+/*
+ * Example 2
+ */
+
+(function () {
+    const example = d3.select('.example2');
+
+    function updateExample(data) {
+        const colourCategories = [0, 1, 2, 3, 4];
+        const qScale = d3.scaleQuantile().domain(data).range(colourCategories);
+        const cScale = d3.scaleOrdinal().domain(colourCategories).range(d3.schemeCategory10);
+        const selection = example
+            .select('.visualisation table')
+            .selectAll('tr')
+            .data(data);
+        const newRows = selection
+            .enter()
+            .append('tr');
+        newRows
+            .append('td')
+            .text((d, i) => `index: ${i}: ${d}`);
+        newRows
+            .append('td')
+            .style('width', '500px')
+            .append('div')
+            .text(d => `${d}`)
+            .style('background-color', d => cScale(qScale(d)))
+            .style('width', d => `${500 / 50 * d}px`);
+        selection.exit().remove();
+        example
+            .select('.rawData')
+            .text(`[${data.join(', ')}]`);
+    }
+
+    example
+        .select('.setDataBtn')
+        .on('click', () => updateExample(createRandomData(1, 10, 50)));
+} ());
